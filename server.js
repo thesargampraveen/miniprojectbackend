f4b46b4f4ff71f3c0f23afa3ab9142b14c0507f8
thesargampraveen <sargampraveen3@gmail.com>
2026-09-18 20:36:59 +0530

    port:

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./src/db/db');
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Bhasha API is running on port ${PORT}`);
   });
 });
